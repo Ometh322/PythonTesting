8db0b0a2db56dc3797875dcca912465d36cb8e6a
Даниил Остроухов <d.ostroukhov@bftcom.com>
2024-03-28 10:49:15 +0300

    Added second test Firefox

--- test_pictures/test_pictures.py
+++ test_pictures/test_pictures.py
@@ -298,3 +298,103 @@ def test_chrome_tram_genre():
                               attachment_type=AttachmentType.PNG)
             driver.close()
             driver.quit()
+
+
+# 2.2 Перейти в “Вышитые картины”, произвести поиск по жанру
+# «Городской пейзаж», открыть подробности картины “Трамвайный путь”,
+# проверить, что стиль картины «Реализм». (Firefox)
+@allure.story("Create a driver, open the window to full screen, go to the website")
+@allure.severity("blocker")
+def test_firefox_tram_genre():
+    with allure.step("Создаем драйвер, открываем окно на полный экран, переходим на сайт"):
+        try:
+            driver = webdriver.Firefox()
+            driver.maximize_window()
+            driver.get("https://artnow.ru")
+        except Exception as e:
+            print(e)
+            with allure.step("Taking a screenshot of the error"):
+                allure.attach(driver.get_screenshot_as_png(), name="firefox_error",
+                              attachment_type=AttachmentType.PNG)
+            driver.close()
+            driver.quit()
+    with allure.step("Open the drop-down list"):
+        try:
+            drop_down_list = driver.find_element(
+                By.CSS_SELECTOR,
+                "#left_container > div > ul:nth-child(2) > li.menu-group.gids > div")
+            drop_down_list.click()
+        except Exception as e:
+            print(e)
+            with allure.step("Taking a screenshot of the error"):
+                allure.attach(driver.get_screenshot_as_png(), name="firefox_error",
+                              attachment_type=AttachmentType.PNG)
+            driver.close()
+            driver.quit()
+    with allure.step("Moving on to the embroidered paintings"):
+        try:
+            paintings = driver.find_element(By.CSS_SELECTOR,
+                                            "#left_container > div > ul:nth-child(2) > li:nth-child(8) > a")
+            paintings.click()
+        except Exception as e:
+            print(e)
+            with allure.step("Taking a screenshot of the error"):
+                allure.attach(driver.get_screenshot_as_png(), name="firefox_error",
+                              attachment_type=AttachmentType.PNG)
+            driver.close()
+            driver.quit()
+    with allure.step("Choosing the Urban Landscape genre"):
+        try:
+            city_landscape = driver.find_element(By.CSS_SELECTOR,
+                                                 "#genrebox > div > label:nth-child(2)")
+            city_landscape.click()
+            time.sleep(3)
+            use = driver.find_element(By.CSS_SELECTOR, "#applymsg")
+            use.click()
+            time.sleep(3)
+        except Exception as e:
+            print(e)
+            with allure.step("Taking a screenshot of the error"):
+                allure.attach(driver.get_screenshot_as_png(), name="firefox_error",
+                              attachment_type=AttachmentType.PNG)
+            driver.close()
+            driver.quit()
+    with allure.step("Looking for a tramway among the paintings"):
+        try:
+            tram_track = driver.find_element(By.CSS_SELECTOR, "#sa_container > div:nth-child(5) > a > div")
+            tram_track.click()
+        except Exception as e:
+            print(e)
+            with allure.step("Taking a screenshot of the error"):
+                allure.attach(driver.get_screenshot_as_png(), name="firefox_error",
+                              attachment_type=AttachmentType.PNG)
+            driver.close()
+            driver.quit()
+    with allure.step("Check the genre of the painting for realism"):
+        try:
+            response = str(re.search(r'Стиль: (.*?)\.', str(driver.page_source)).group())
+        except Exception as e:
+            print(e)
+            with allure.step("Taking a screenshot of the error"):
+                allure.attach(driver.get_screenshot_as_png(), name="firefox_error",
+                              attachment_type=AttachmentType.PNG)
+            driver.close()
+            driver.quit()
+    try:
+        assert "реализм" in response
+    except AssertionError("Current genre - not realism"):
+        with allure.step("Taking a screenshot of the error"):
+            allure.attach(driver.get_screenshot_as_png(), name="firefox_error", attachment_type=AttachmentType.PNG)
+        driver.close()
+        driver.quit()
+    with allure.step("Closing driver"):
+        try:
+            driver.close()
+            driver.quit()
+        except Exception as e:
+            print(e)
+            with allure.step("Taking a screenshot of the error"):
+                allure.attach(driver.get_screenshot_as_png(), name="firefox_error",
+                              attachment_type=AttachmentType.PNG)
+            driver.close()
+            driver.quit()
